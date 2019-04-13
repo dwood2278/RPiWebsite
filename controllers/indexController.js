d9f33252c0896610763d40c817af58cdc930f75b
Don Wood <donwood@gmail.com>
2019-04-13 01:44:02 -0500

    Changed to fetch the sense hat data from the API instead of preloading it into the template. This is so the template doesn't slowly load while waiting for the python script to execute.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,15 +3,5 @@ var senseHat = require ('../raspberry_pi/senseHat.js');
 
 //Home page of the site.
 exports.index = function (req, res, next) {
-
-    //Get promise for sense hat data
-    var senseHatDataPromise = senseHat.getSenseHatData(); 
-
-    //Use the promise to render the page on success
-    senseHatDataPromise.then(function(result) {
-        res.render('pages/index', { title: 'Galileo', senseHatData: result});
-    },
-    function(err) {
-        console.log(err);
-    });
+    res.render('pages/index', { title: 'Galileo'});
 };
