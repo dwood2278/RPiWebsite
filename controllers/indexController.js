2c5cd9e6f101d60a3ea21223676b5b55b6c12164
Don Wood <donwood@gmail.com>
2019-04-14 15:56:40 -0500

    Added an image and a favicon.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,5 +3,5 @@ var senseHat = require ('../raspberry_pi/senseHat.js');
 
 //Home page of the site.
 exports.index = function (req, res, next) {
-    res.render('pages/index', { title: 'Galileo'});
+    res.render('pages/index', { title: 'Raspberry Pi Galileo' ,raspberryPiName: 'Galileo'});
 };
